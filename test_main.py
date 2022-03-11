8f284ed38c9dbaf1abb6dc749fc04068a672813c
Lucas Derksen <luc.derksen@gmail.com>
2022-03-11 20:21:37 +0100

    added test-file

---
+++ test_main.py
@@ -0,0 +1,13 @@
+from main import app
+import pytest
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_index(client):
+    response = client.get("/")
+    assert response.status_code == 200
